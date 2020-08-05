6eccb91f5afc51694409c3acb5b368322a6b1761
ifaidros <ifaidros@gmail.com>
2020-08-05 13:48:10 +0100

    1st version

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//console.log('script file loaded')
+console.log('script file loaded')
 
 const h1Title = document.querySelector('h1')
 h1Title.textContent = new Date().getFullYear()
 
-const btnOne = document.querySelector('#btnOne')
-const btnTwo = document.querySelector('#btnTwo')
+// const btnOne = document.querySelector('#btnOne')
+// const btnTwo = document.querySelector('#btnTwo')
 
 
-btnOne.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log('Button One clicked')
-})
+// btnOne.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log('Button One clicked')
+// })
 
-btnTwo.addEventListener('click', (e) => {
-    e.preventDefault()
-    console.log('Button Two clicked')
-})
+// btnTwo.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log('Button Two clicked')
+// })
 
